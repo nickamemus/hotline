85174f54f5e2234a3f27e244c95ac55da44cda64
nickamemus <129408586+nickamemus@users.noreply.github.com>
2026-02-02 12:44:22 -0700

    Update jasminnod.sh

--- jasminnod.sh
+++ jasminnod.sh
@@ -3,7 +3,7 @@
 export LOG_PATH=$HOME/jasmin/logs
 export JASMIN_RESOURCE_DIR=$HOME/jasmin-src/resource
 
-python $HOME/jasmin-src/jasmind.py \
+python3 $HOME/jasmin-src/jasmind.py \
   --http-api \
   --enable-interceptor-client \
   --enable-dlr-thrower \
